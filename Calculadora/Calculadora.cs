ae68767379718aa1dcaf275c2e9a5f2208663819
Wendley Silva <110824974+DileyS@users.noreply.github.com>
2023-02-13 11:36:28 -0300

    Update Calculadora.cs

--- Calculadora/Calculadora.cs
+++ Calculadora/Calculadora.cs
@@ -1,5 +1,5 @@
-﻿using System;
-using System.Collections.Generic;
+using System;
+using System.Collections.Generic; //Representação de uma coleção somente leitura genérica de pares chave/valor.
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -8,80 +8,164 @@ namespace Calculadora
 {
     internal class Calculadora
     {
-        static void Main(string[] args)
+        
+        static void Main()
         {
+            int num1, num2, resultado = 0;
+            int operacao = 0;
             Console.WriteLine("Qual operação deseja fazer:");
             Console.WriteLine("1 - Adição");
             Console.WriteLine("2 - Subtração");
             Console.WriteLine("3 - Multiplicação");
-            Console.WriteLine("4 - Divisão \n");
-
-            //Quando digitamos um numero no console, ele entrara como string mas na conversao ele voltaram como um numero inteiro.
-            int operacao = int.Parse(Console.ReadLine());
-
-            Console.WriteLine("Digite o primeiro numero: ");
-            int num1 = int.Parse(Console.ReadLine());
-
-            Console.WriteLine("Digite o segundo numero: ");
-            int num2 = int.Parse(Console.ReadLine());
-            Console.Clear();
-            int resultado = 0;
-
+            Console.WriteLine("4 - Divisão");
+            Console.WriteLine("5 - Resto da Divisão");
+            Console.WriteLine("6 - Potenciação");
+            Console.WriteLine("0 - Sair \n");
+           
+            operacao = Convert.ToInt32(Console.ReadLine());
             switch (operacao)
             {
                 //Como se fosse uma condicao se o usuario digitar "1" que seria Adicao, o que sera exibido sera a soma dos dois numeros.
                 case 1:
                     {   //Se o usuario digita 1 o resultado sera o metodo "Adicao" que e "num1 + num2".
-                        Console.WriteLine("Adição:");
-                        Console.WriteLine("=============");
-                        resultado = Adicao(num1, num2);
+                        Console.WriteLine("Adição:");          
                         break;
                     }
                 case 2:
-                    {   
-                        resultado = Subtracao(num1, num2);
+                    {
+                        Console.WriteLine("Subtração:");                       
                         break;
                     }
                 case 3:
                     {
-                        resultado = Multiplicacao(num1, num2);
+                        Console.WriteLine("Multiplicação:");                        
                         break;
                     }
                 case 4:
                     {
-                        resultado = Divisao(num1, num2);
+                        Console.WriteLine("Divisão:");                       
                         break;
                     }
-                //caso nao seja nenhum numero de 1 a 4 
+                case 5:
+                    {
+                        Console.WriteLine("Resto da Divisão:");                        
+                        break;
+                    }
+                case 6:
+                    {
+                        Console.WriteLine("Potenciação:");                       
+                        break;
+                    }
+                case 0:
+                    {
+                        Console.WriteLine("Obrigado por utilizar nossa calculadora !");
+                        break;
+                    }
+                //caso nao seja nenhum numero de 1 a 6 
                 default:
+                    Console.Clear();
                     Console.WriteLine("Numero invalido, digite outro numero.");
+                    Console.WriteLine("=====================================");
+                    Main();
                     break;
             }
-            //Quando e colocado um numero dentro da {} e exibido a variavel declarada no fim da condicao.
-            Console.WriteLine("O resultado da operacao com os numeros {0} e {1} é: {2}", num1, num2, resultado);
-            Console.ReadLine();
-        }
-        //Metodo novo nao tem nada haver com o metodo de cima.
-        //Metodos somente usados para retornar os valores e as operacoes solicitadas no exercicio.
-        public static int Adicao (int num1, int num2)
-        {
-            int resultado = num1 + num2;
-            return resultado;
-        }
-        public static int Subtracao(int num1, int num2)
-        {
-            int resultado = num1 - num2;
-            return resultado;
-        }
-        public static int Multiplicacao(int num1, int num2)
-        {
-            int resultado = num1 * num2;
-            return resultado;
-        }
-        public static int Divisao(int num1, int num2)
-        {
-            int resultado = num1 / num2;
-            return resultado;
+            if (operacao == 1)
+            {
+                Console.WriteLine("===========================");
+                Console.WriteLine("Digite o primeiro numero: ");
+                num1 = int.Parse(Console.ReadLine());
+                Console.WriteLine("===========================");
+                Console.WriteLine("Digite o segundo numero: ");
+                num2 = int.Parse(Console.ReadLine());
+                resultado = num1 + num2;
+                Console.WriteLine("===========================");
+                Console.WriteLine("O resultado da operação foi de: {0}", resultado);
+                Thread.Sleep(2000);
+                Console.Clear();
+                Main();
+            }
+            if (operacao == 2)
+            {
+                Console.WriteLine("===========================");
+                Console.WriteLine("Digite o primeiro numero: ");
+                num1 = int.Parse(Console.ReadLine());
+                Console.WriteLine("===========================");
+                Console.WriteLine("Digite o segundo numero: ");
+                num2 = int.Parse(Console.ReadLine());
+                resultado = num1 - num2;
+                Console.WriteLine("===========================");
+                Console.WriteLine("O resultado da operação foi de: {0}", resultado);
+                Thread.Sleep(2000);
+                Console.Clear();
+                Main();
+            }
+            if (operacao == 3)
+            {
+                Console.WriteLine("===========================");
+                Console.WriteLine("Digite o primeiro numero: ");
+                num1 = int.Parse(Console.ReadLine());
+                Console.WriteLine("===========================");
+                Console.WriteLine("Digite o segundo numero: ");
+                num2 = int.Parse(Console.ReadLine());
+                resultado = num1 * num2;
+                Console.WriteLine("===========================");
+                Console.WriteLine("O resultado da operação foi de: {0}", resultado);
+                Thread.Sleep(2000);
+                Console.Clear();
+                Main();
+            }
+            if (operacao == 4)
+            {
+                Console.WriteLine("===========================");
+                Console.WriteLine("Digite o primeiro numero: ");
+                num1 = int.Parse(Console.ReadLine());
+                Console.WriteLine("===========================");
+                Console.WriteLine("Digite o segundo numero: ");
+                num2 = int.Parse(Console.ReadLine());
+                if(num2 == 0)
+                {
+                    Console.WriteLine("Não é possível dividir por zero.");
+                    Console.WriteLine("===========================");
+                    Console.WriteLine("Digite o segundo numero: ");
+                    num2 = int.Parse(Console.ReadLine());
+                }
+                resultado = num1 / num2;
+                Console.WriteLine("===========================");
+                Console.WriteLine("O resultado da operação foi de: {0}", resultado);
+                Thread.Sleep(2000);
+                Console.Clear();
+                Main();
+            }
+            if (operacao == 5)
+            {
+                Console.WriteLine("===========================");
+                Console.WriteLine("Digite o primeiro numero: ");
+                num1 = int.Parse(Console.ReadLine());
+                Console.WriteLine("===========================");
+                Console.WriteLine("Digite o segundo numero: ");
+                num2 = int.Parse(Console.ReadLine());
+                resultado = num1 % num2;
+                Console.WriteLine("===========================");
+                Console.WriteLine("O resultado da operação foi de: {0}", resultado);
+                Thread.Sleep(2000);
+                Console.Clear();
+                Main();
+            }
+            if (operacao == 6)
+            {
+                Console.WriteLine("===========================");
+                Console.WriteLine("Digite o primeiro numero: ");
+                num1 = int.Parse(Console.ReadLine());
+                Console.WriteLine("===========================");
+                Console.WriteLine("Digite o segundo numero: ");
+                num2 = int.Parse(Console.ReadLine());
+                resultado = (int)Math.Pow(num1,num2);
+                Console.WriteLine("===========================");
+                Console.WriteLine("O resultado da operação foi de: {0}", resultado);
+                Thread.Sleep(2000);
+                Console.Clear();
+                Main();
+            }
         }
     }
 }
